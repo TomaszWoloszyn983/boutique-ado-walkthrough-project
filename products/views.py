c39889911979a2e74b77fdf000de31bd490062ab
Tomasz <tomaszwoloszyn983@gmail.com>
2022-12-29 07:59:30 +0000

    Fixed Edit and Delete product bug

--- products/views.py
+++ products/views.py
@@ -126,7 +126,7 @@ def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store ownes can do that.')
-    return redirect(reverse('home'))
+        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
